22b7885c815e06716be6332ef79ca57661ee8bcf
umeboshi2201 <umeume2201@gmail.com>
2018-10-25 14:40:33 +0900

    Add projects by express-generator

---
+++ views/error.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+ 
+class DefaultLayout extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>This is error page.</body>
+      </html>
+    );
+  }
+}
+ 
+module.exports = DefaultLayout;
